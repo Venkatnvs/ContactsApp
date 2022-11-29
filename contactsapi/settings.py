1a56bda3acb9054e905eba0aa1e980517b0ec232
Venkatnvs <venkatnvs2005@gmail.com>
2022-11-29 17:34:05 +0530

    heroku chages made

--- contactsapi/settings.py
+++ contactsapi/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,3 +143,5 @@ SWAGGER_SETTINGS = {
         }
     },
 }
+
+django_heroku.settings(locals())
