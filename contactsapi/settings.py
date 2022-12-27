ac32a8c29b1a4b5b7b50586d8a8de7b13140efca
Venkatnvs <97899253+Venkatnvs@users.noreply.github.com>
2022-12-27 15:40:04 +0530

    Update settings.py

--- contactsapi/settings.py
+++ contactsapi/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'p2w)_k!#@$c*u*%m%pc-gi(bzjxwcn#vf^7kg7sz*=3zeb!e@6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["nvs-contacts.onrender.com"]
 
 
 # Application definition
@@ -125,6 +126,7 @@ JWT_SECRET_KEY = config('JWT_SECRET_KEY')
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 
 REST_FRAMEWORK = {
